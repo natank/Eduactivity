ad4ed6603cfc0e2450bba01aea3effad673e2c29
Nati <natan.kamusher@gmail.com>
2019-10-28 08:17:47 +0200

    Added react hookup with stateful reloading

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import Counter from './counter';
-ReactDOM.render(
-    <Counter />,
+import { AppContainer } from "react-hot-loader"
+function render(Component){
+   ReactDOM.render(
+        <AppContainer >
+        <Component />,
+    </AppContainer>,
     document.getElementById('react-root')
-)
+    )
+}
+    
+render(Counter)
+
+if(module.hot){
+    module.hot.accept("./counter.js", ()=>{
+        const NewCounter = require("./counter.js").default
+        render(NewCounter);
+    })
+}
