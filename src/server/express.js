a6cf939a23d5df2132b2efe4fcc3ead462438675
Nati <natan.kamusher@gmail.com>
2019-10-29 19:09:38 +0200

    Added shop router and admin router

--- src/server/express.js
+++ src/server/express.js
@@ -5,8 +5,8 @@ const server = express();
 
 const webpack = require("webpack");
 const config = require("../../config/webpack.dev.js");
-const compiler = webpack(config);
 
+const compiler = webpack(config);
 const webpackDevMiddleware = require("webpack-dev-middleware")(
   compiler,
   {
@@ -20,17 +20,19 @@ const webpackDevMiddleware = require("webpack-dev-middleware")(
 
 )
 const webpackHotMiddleware = require("webpack-hot-middleware")(compiler);
+const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./routes/admin")
+
 
 server.use(webpackDevMiddleware);
 server.use(webpackHotMiddleware);
-
+server.use
 const staticMiddleware = express.static("/dist");
 server.use(staticMiddleware);
 
-server.get('', (req, res, next) => {
-  debugger
-  res.sendFile(__dirname + '/dist/home.html')
-})
+server.use('/', shopRoutes)
+server.use('/admin', adminRoutes)
+
 
 server.listen(8080, () => {
   console.log("Server is listening")
