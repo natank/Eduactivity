ef5c20f66d3d137b5b54859d75848df293a5c3b7
Nati <natan.kamusher@gmail.com>
2020-01-07 16:34:16 +0200

    Added axios

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,13 +6,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
     form.submit();
   })
 
+  document.querySelectorAll('.bi-heart').forEach(elem=>elem.addEventListener('click', product.wishlistProduct))
+  document.querySelectorAll('.bi-heart-fill').forEach(elem=>elem.addEventListener('click', product.unWishlistProduct))
 
 })
 
 let product = (function () {
   return {
-    wiishlistProduct: function (prodId) {
-      alert("product wishlisted")
+    wishlistProduct: function () {
+      const heart = event.target;
+      const id = heart.closest('.card').getAttribute('data-id');
+      const heartFill = heart.closest('.card').querySelector('.bi-heart-fill')
+      heart.style.visibility='hidden';
+      heartFill.style.visibility = 'visible';
+      //alert(`product ${id} wishlisted`)
+    },
+    unWishlistProduct: function(){
+      const heartFill = event.target;
+      const id = heartFill.closest('.card').getAttribute('data-id');
+      const heart = heartFill.closest('.card').querySelector('.bi-heart')
+      heartFill.style.visibility='hidden';
+      heart.style.visibility = 'visible';
     }
   }
 })()
