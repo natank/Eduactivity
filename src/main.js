3a0bc40c24feb6f8761575ae5280feb17b91c296
Nati <natan.kamusher@gmail.com>
2020-06-11 16:02:58 +0300

    ready for production

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
-import('./fonts/ArbeiBerry.otf');
-import('./fonts/Chewy-Regular.ttf');
-import('./fonts/Hellofreeday.otf');
-import('./fonts/NunitoSans-Regular.ttf');
+
 require("./styles/main.scss");
 require("bootstrap");
 require("./scripts/index");
